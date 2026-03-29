8755ae58327ff85f7d61bb267f9b872c2b2174ba
rokayaraka <rokayaraka251@gmail.com>
2026-03-29 21:54:54 +0600

    addNewTask1.1

--- lib/taskScreens/new_task_screen.dart
+++ lib/taskScreens/new_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_astad/screens/add_new_task.dart';
 import 'package:task_manager_astad/widgets/taskCard.dart';
 import 'package:task_manager_astad/widgets/taskCount_by_status.dart';
 import 'package:task_manager_astad/widgets/tm_appbar.dart';
@@ -47,6 +48,12 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>AddNewTask()));
+      },
+      
+      child: Icon(Icons.add),),
+    
     );
   }
 }
--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -2,4 +2,6 @@ class Urls{
   static String _baseUrl = 'https://task.teamrabbil.com/api/v1';
   static String SignUpUrl='$_baseUrl/registration';
   static String LogInUrl='$_baseUrl/login';
+  static String AddTaskUrl='$_baseUrl/createTask';
+
 }
